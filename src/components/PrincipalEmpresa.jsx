0d02fb2282dbf00749e1b9e565980b8a646cd851
JafetCid <jafetcid9@gmail.com>
2023-07-12 09:27:00 -0600

    Segunda vista

---
+++ src/components/PrincipalEmpresa.jsx
@@ -0,0 +1,69 @@
+import { Button } from "./atoms/Button"
+import { Navbar } from "./organisms/Navbar"
+import img1 from '../assets/1.png';
+import img2 from '../assets/2.png';
+// import img3 from '../assets/3.png';
+// import img4 from '../assets/Captura.png';
+
+export const PrincipalEmpresa = () => {
+    return (
+        <>
+            <Navbar/>
+
+            <div className="card">
+                <div className="card-image">
+                    <figure className="image is-4by3">
+                    <img src={img1} alt="Placeholder image"/>
+                    </figure>
+                </div>
+            </div>
+
+            <h3 className="subtitle mt-4">
+                Ahorra costos y tiempo en tus procesos <br />
+                de seleccion de forma rapída y fácil
+            </h3>
+
+            <Button TextName="Publicar un oferta"/>
+
+            <div className="card2 mt-5">
+                <div className="card-image">
+                    <figure className="image is-4by3">
+                    <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image"/>
+                    </figure>
+                </div>
+            </div>
+    
+            <h2 className="title2 mt-5">
+                Publica tu oferta <br /> 
+                en Upperjob
+            </h2>
+
+            <Button TextName="Encontrar candidatos"/>
+
+            <h2 className="subtitle2 mt-5 "> 
+                Localiza los mejores candidatos en la <br />
+                web de empleo en Latinoamérica 
+            </h2>
+
+            <div className="card">
+                <div className="card-image">
+                    <figure className="image is-4by3">
+                    <img src={img2} alt="Placeholder image"/>
+                    </figure>
+                </div>
+            </div>
+
+            <h2 className="h2 mt-4">Atrae al candidato que necesitas</h2>
+
+            <div className="card">
+                <div className="card-image">
+                    <figure className="image is-4by3">
+                    <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image"/>
+                    </figure>
+                </div>
+            </div>
+
+            <Button TextName="Comienza ahora"/>
+        </>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import 'bulma/css/bulma.min.css'
 import './main.css'
+import './style.css'
+import { PrincipalEmpresa } from './components/PrincipalEmpresa.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <PrincipalEmpresa />
   </React.StrictMode>,
 )
