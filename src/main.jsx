cf12d9d2453e6438baae9cfeffd382f8ac49a095
angelG2903 <angel.gabriel-1999@hotmail.com>
2023-07-12 14:53:36 -0600

    good

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import 'bulma/css/bulma.min.css'
 import './main.css'
-<<<<<<< HEAD
-import './style.css'
+import './components/Pages/styles/style.css'
 import { BrowserRouter } from 'react-router-dom'
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
@@ -12,15 +12,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </BrowserRouter>
 
-=======
-import './components/Pages/styles/style.css'
-import './components/Pages/styles/informes.css'
-import { InformesButton } from './components/Pages/InformesButton.jsx'
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <InformesButton/>
-  </React.StrictMode>,
->>>>>>> ab79d88128912142d6e3f38ac0d178dce298e675
 )
