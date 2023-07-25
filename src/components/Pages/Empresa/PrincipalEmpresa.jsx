c3321ebf207cb954d7671074d0b826ca591de638
angelG2903 <angel.gabriel-1999@hotmail.com>
2023-07-25 15:48:52 -0600

    ya quedo

--- src/components/Pages/Empresa/PrincipalEmpresa.jsx
+++ src/components/Pages/Empresa/PrincipalEmpresa.jsx
@@ -8,64 +8,66 @@ export const PrincipalEmpresa = () => {
     return (
         <>
             <section className="margenes">
-                <Navbar/>
-            </section>
+                <Navbar />
 
-                <figure className="image is-5by4 img">
-                    <img src={imagenes.img1}></img>
-                    <div className="titleImg">
-                        <h2>¡Únete a nosotros y publica tus ofertas !</h2>
+                <article className="content-art1">
+                    <div className="content-img1-text">
+                        <h2 className="titleImg">¡Únete a nosotros y publica tus ofertas!</h2>
+                        <img src={imagenes.img1} className="img1-text"/>
                     </div>
-                </figure>
 
-                <section className="margenes">
-                <h3 className="subtitle mt-4">
-                    Ahorra costos y tiempo en tus procesos <br />
-                    de seleccion de forma rapída y fácil
-                </h3>
+                    <div className="content-sub-bu">
+                        <h3 className="subtitle mt-4">
+                            Ahorra costos y tiempo en tus procesos <br />
+                            de seleccion de forma rapída y fácil
+                        </h3>
 
-                <Button TextName="Publicar una oferta"/>
+                        <Button TextName="Publicar una oferta" />
 
-                <Card2/>
-        
-                <h2 className="title2 mt-5">
-                    Publica tu oferta <br /> 
-                    en Upperjob
-                </h2>
-
-                <Button TextName="Encontrar candidatos"/>
-
-                <h2 className="subtitle2 mt-5 "> 
-                    Localiza los mejores candidatos en la <br />
-                    web de empleo en Latinoamérica 
-                </h2>
-
-                <figure className="image is-4by3 img2">
-                    <img src={imagenes.img2}></img>
-                </figure>
-
-                <h2 className="h2 mt-4">Atrae al candidato que necesitas</h2>
-
-                <div className="car">
-                    <div className="cardF">
-                        <img src={imagenes.img3} alt="Placeholder image"/>
                     </div>
-                    <div className="ic">
 
-                        <i className="fa-regular fa-lightbulb"></i>
-                        <p>
-                            Verifica sus habilidades
-                        </p>
-
-                        <i className="fa-solid fa-users"></i>
-                        <p>
-                            Organiza tus candidatos
-                        </p>
-                            
+                    <div className="content-ti2-car2">
+                        <h2 className="title2 mt-5">
+                            Publica tu oferta <br />
+                            en Upperjob
+                        </h2>
+                        <Card2 />
+                        <Button TextName="Encontrar candidatos" style={"mt-5"} />
                     </div>
-                </div>
+                </article>
 
-                <Button TextName="Comienza ahora"/>
+                <article className="conetent-art2">
+                    <div className="conetnt-sub2-img2">
+                        <h2 className="subtitle2 mt-5 ">
+                            Localiza los mejores candidatos en la <br />
+                            web de empleo en Latinoamérica
+                        </h2>
+                        <img src={imagenes.img2} className="img2-sub"/>
+                    </div>
+
+                    <div className="content-t-c-bu">
+                        <h2 className="h2 mt-4">Atrae al candidato que necesitas</h2>
+                        <div className="car">
+                            <img className="cardF" src={imagenes.img3} />
+                            <div className="ic">
+
+                                <i className="fa-regular fa-lightbulb"></i>
+                                <p>
+                                    Verifica sus habilidades
+                                </p>
+
+                                <i className="fa-solid fa-users"></i>
+                                <p>
+                                    Organiza tus candidatos
+                                </p>
+
+                            </div>
+                        </div>
+
+                        <Button TextName="Comienza ahora" />
+                    </div>
+                </article>
+
             </section>
 
         </>
